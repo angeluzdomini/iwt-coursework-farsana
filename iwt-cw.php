2064d9d1dd72c44729dd233fc6cdb2b091a96a94
Farsana Majeed Kallungal <farsana.kallungal@transferwise.com>
2023-03-07 09:43:50 +0000

    Search logic added

--- iwt-cw.php
+++ iwt-cw.php
@@ -1,82 +1,123 @@
-<!doctype html>
-<html class="no-js" lang="">
-<body>
 <?php
-$mens_json = file_get_contents('resources/mens-grand-slam-winners.json');
-$womens_json = file_get_contents('resources/womens-grand-slam-winners.json');
-$decoded_mens_json = json_decode($mens_json, true);
-$decoded_womens_json = json_decode($womens_json, true);
-$gender = "womens";
-if ($gender == "mens") {
-  $results = getSearchResult($decoded_mens_json, 2020, "equals", "Wimbledon", "Andy", "");
-} elseif ($gender == "womens") {
-  $results = getSearchResult($decoded_womens_json, 2014, "after", "U.S. Open", "", "");
-} else {
-  $decoded_mixed_json = array_merge($decoded_mens_json, $decoded_womens_json);
-  $results = getSearchResult($decoded_mixed_json, 2020, null, "Wimbledon", "", "mi");
+header('Content-Type: application/json; charset=utf-8');
+
+$year = $_GET["year"];
+$year_op = $_GET["year-op"];
+$tournament = $_GET["tournament"];
+$file = $_GET["file"];
+$winner = $_GET["winner"];
+$runner_up = $_GET["runnerup"];
+$results = search($year, $year_op, $tournament, $winner, $runner_up, $file);
+echo $results;
+
+function search($year, $year_op, $tournament, $winner, $runner_up, $file)
+{
+  $mens_json = file_get_contents('resources/mens-grand-slam-winners.json');
+  $womens_json = file_get_contents('resources/womens-grand-slam-winners.json');
+  $decoded_mens_json = json_decode($mens_json, true);
+  $decoded_womens_json = json_decode($womens_json, true);
+  switch ($file) {
+    case 'mens-grand-slam-winners.json':
+      $results = getSearchResult($decoded_mens_json, $year, $year_op, $tournament, $winner, $runner_up);
+      return json_encode($results);
+    case 'womens-grand-slam-winners.json':
+      $results = getSearchResult($decoded_womens_json, $year, $year_op, $tournament, $winner, $runner_up);
+      return json_encode($results);
+    case 'any':
+      $decoded_mixed_json = array_merge($decoded_mens_json, $decoded_womens_json);
+      $results = getSearchResult($decoded_mixed_json, $year, $year_op, $tournament, $winner, $runner_up);
+      return json_encode($results);
+    default:
+      $error_message = new stdClass();
+      $error_message->error = "Error! Invalid value for file.";
+      return json_encode($error_message);
+  }
 }
-printResult($results);
 
 function getSearchResult($data_json, $search_year, $year_condition, $search_tournament, $search_winner, $search_runnerup)
 {
-  print_r($search_year);
-  echo ":";
-  print_r($year_condition);
-  echo ":";
-  print_r($search_tournament);
-  echo ":";
-  print_r($search_winner);
-  echo ":";
-  print_r($search_runnerup);
-  echo "<br>";
-
   $results = array();
-
+  $error_message = new stdClass();
   foreach ($data_json as $item) {
     $year = $item['year'];
     $tournament = $item['tournament'];
     $winner = $item['winner'];
     $runnerup = $item['runner-up'];
-    if (checkYear($year, $search_year, $year_condition)) {
-      if (checkTournament($tournament, $search_tournament)) {
-        if (checkWinner($winner, $search_winner) && checkRunnerup($runnerup, $search_runnerup)) {
-          $results[] = $item;
+    $check_year = checkYear($year, $search_year, $year_condition);
+    if ($check_year === true) {
+      $check_tournament = checkTournament($tournament, $search_tournament);
+      if ($check_tournament === true) {
+        $check_winner = checkWinner($winner, $search_winner);
+        if ($check_winner === true) {
+          $check_runner_up = checkRunnerup($runnerup, $search_runnerup);
+          if ($check_runner_up === true) {
+            $results[] = $item;
+          } elseif ($check_runner_up !== false) {
+            $error_message->error = $check_runner_up;
+            return $error_message;
+          }
+        } elseif ($check_winner !== false) {
+          $error_message->error = $check_winner;
+          return $error_message;
         }
+      } elseif ($check_tournament !== false) {
+        $error_message->error = $check_tournament;
+        return $error_message;
       }
+    } elseif ($check_year !== false) {
+      $error_message->error = $check_year;
+      return $error_message;
     }
   }
   return $results;
 }
 
-function checkYear($item_year, $search_year, $year_condition): bool
+function checkYear($item_year, $search_year, $year_condition): bool|string
 {
+  if (!is_numeric($item_year)) {
+    return "Error! Year should be only numbers.";
+  }
+  if ($year_condition != null && $search_year == null) {
+    return "Error! Search year cannot be empty with a condition.";
+  }
+  if ($search_year == null && $year_condition != null) {
+    return "Error! Search condition cannot be empty with a year.";
+  }
   switch ($year_condition) {
-    case "equals":
+    case "=":
       return $item_year == $search_year;
-    case "after":
+    case ">":
       return $item_year > $search_year;
-    case "before":
+    case "<":
       return $item_year < $search_year;
-    case null:
-      if ($search_year == null) {
-        return  true;
-      }
+    default:
+      return "Error! Invalid year condition.";
   }
-  return false;
 }
 
-function checkTournament($tournament, $search_tournament): bool
+function checkTournament($tournament, $search_tournament): bool|string
 {
-  if ($search_tournament == "any" || $search_tournament == $tournament) {
-    return true;
+  switch ($search_tournament) {
+    case 'any':
+      return true;
+    case 'Australian Open':
+    case 'U.S. Open':
+    case 'French Open':
+    case 'Wimbeldon':
+      break;
+    default:
+      return 'Error! Invalid tournament name.';
   }
   return false;
 }
 
-function checkWinner($winner, $search_winner): bool
+function checkWinner($winner, $search_winner): bool|string
 {
   if ($search_winner == "") {
     return true;
+  }
+  if (preg_match('~[0-9]+~', $search_winner)) {
+    return "Error! Winner name cannot contain numbers.";
   }
   if (stripos($winner, $search_winner) !== false) {
     return true;
@@ -84,10 +125,13 @@ function checkWinner($winner, $search_winner): bool
   return false;
 }
 
-function checkRunnerup($runnerup, $search_runnerup): bool
+function checkRunnerup($runnerup, $search_runnerup): bool|string
 {
   if ($search_runnerup == "") {
     return true;
+  }
+  if (preg_match('~[0-9]+~', $search_runnerup)) {
+    return "Error! Runner-up name cannot contain numbers.";
   }
   if (stripos($runnerup, $search_runnerup) !== false) {
     return true;
@@ -95,25 +139,4 @@ function checkRunnerup($runnerup, $search_runnerup): bool
   return false;
 }
 
-function printResult($results){
-  echo "<b>RESULTS</b><br><br>";
-  foreach ($results as $result_item) {
-    $year = $result_item['year'];
-    $tournament = $result_item['tournament'];
-    $winner = $result_item['winner'];
-    $runnerup = $result_item['runner-up'];
-    print_r($year);
-    echo " : ";
-    echo " : ";
-    print_r($tournament);
-    echo " : ";
-    print_r($winner);
-    echo " : ";
-    print_r($runnerup);
-    echo "<br>";
-  }
-}
-
 ?>
-</body>
-</html>
--- js/iwt-cw.js
+++ js/iwt-cw.js
@@ -1,4 +1,23 @@
-function clearResult(elementID)
-{
+$("#search-form").submit(function () {
+  var formData = $("form#search-form").serialize();
+  var request = $.getJSON('./iwt-cw.php?' + formData);
+  request.done(function (results) {
+    console.log(results)
+    if (results.error != undefined) {
+      alert(results.error);
+    } else {
+      alert(results[0].year)
+      // $.each(results, function (i, item) {
+      //   var $tr = $('<tr>').append(
+      //     $('<td>').text(item.rank),
+      //     $('<td>').text(item.content),
+      //     $('<td>').text(item.UID)
+      //   ).appendTo('#result-table');
+      //   console.log($tr.wrap('<p>').html());
+    }
+  });
+});
+
+function clearResult(elementID) {
   document.getElementById(elementID).innerHTML = "";
 }
